3e6bee1a1be04eada4d58a08024a076592e4c248
jpgaertner <jph.gaertner@gmail.com>
2024-07-30 13:01:52 +0200

    add get_lkf_map function

--- functions/statistics_functions.py
+++ functions/statistics_functions.py
@@ -1,5 +1,13 @@
 import numpy as np
+import xarray as xr
 import os
+import sys
+sys.path.append('/home/a/a270230/LKF/lkf_tools/lkf_tools/')
+from dataset import DoG_leads
+from rgps import mSSMI
+import matplotlib.pyplot as plt
+import cartopy
+import skimage.morphology
 
 xticks = np.array([31,59,90,120,151,181,212,243,273,304,334,365])
 xticks_minor = np.array([16,45,75,105,136,166,197,228,258,289,319,350])
@@ -46,6 +54,7 @@ def get_tracks(datasets):
     
     tracks = []
     for lkf_data in datasets:
+        print(lkf_data.netcdf_file)
     
         tracks_y = []
         tracks_list = os.listdir(lkf_data.track_output_path)
@@ -264,3 +273,144 @@ def coarse_graining(field, coarse_grid_box_size_km, res_km):
     field = np.concatenate(assemble, axis=0)
     
     return field
+
+def get_lkf_pixels(path_to_ncfile, i, dog_thres=0.01, aice_thresh=0, min_kernel=1, max_kernel=5, red_fac=1, skeleton_kernel=0, use_eps=True, plot=True, vmax=[0.4,0.5]):
+    '''
+    parameters to adjust (i is the timestep):
+    dog_thres   : threshold in the DoG filtered image for a feature to be marked as LKF
+                (default = 0.01 units of deformation)
+    aice_thresh : threshold for the ice concentration below which the pixel is not marked
+                as lkf pixel are not used (default = 0)
+    min_kernel  : smallest scale of features to be detected (default = 1 pixel)
+    max_kernel  : largest scale of features to be detected (default = 5 pixel)
+                (with this, the background deformation is calculated:
+                DoG filter = blurred image using min_kernel - blurred image using max_kernel)
+    red_fac     : spatial filtering of the deformation rate field. only every red_fac pixel
+                is usd for the lkf detection (default = 1)
+    use_eps     : flag for using the total deformation (if True, default)
+                or its natural logarithm and a histogram equalization (if False)
+                (the latter highlights local differences across scales and thus enhances
+                contrast in regions of low deformation)
+
+    lkf_thin (return value): a 2D map being 1 at lkf pixels and 0 everywhere else
+    '''
+
+    # read file and store variables
+    data = xr.open_dataset(path_to_ncfile)
+    time = data.time
+    lon = data.ULON
+    lat = data.ULAT
+    lon = lon.where(lon<=1e30); lat = lat.where(lat<=1e30);
+    lon = lon.where(lon<180,other=lon-360)
+    uice = np.array(data.U[i,:,:])
+    vice = np.array(data.V[i,:,:])
+    aice = np.array(data.A[i,:,:])
+
+    # calculate grid cell sizes
+    m = mSSMI()
+    x,y = m(lon,lat)
+    dxu = np.sqrt((x[:,1:]-x[:,:-1])**2 + (y[:,1:]-y[:,:-1])**2)
+    dxu = np.concatenate([dxu,dxu[:,-1].reshape((dxu.shape[0],1))],axis=1)
+    dyu = np.sqrt((x[1:,:]-x[:-1,:])**2 + (y[1:,:]-y[:-1,:])**2)
+    dyu = np.concatenate([dyu,dyu[-1,:].reshape((1,dyu.shape[1]))],axis=0)
+    
+    # calculate strain rates, given in 1/day
+    dudx = ((uice[2:,:]-uice[:-2,:])/(dxu[:-2,:]+dxu[1:-1,:]))[:,1:-1]
+    dvdx = ((vice[2:,:]-vice[:-2,:])/(dxu[:-2,:]+dxu[1:-1,:]))[:,1:-1]
+    dudy = ((uice[:,2:]-uice[:,:-2])/(dyu[:,:-2]+dyu[:,1:-1]))[1:-1,:]
+    dvdy = ((vice[:,2:]-vice[:,:-2])/(dyu[:,:-2]+dyu[:,1:-1]))[1:-1,:]
+
+    # calculate divergence, shear, vorticity, and total deformation
+    div = (dudx + dvdy) * 3600. *24.
+    shr = np.sqrt((dudx-dvdy)**2 + (dudy + dvdx)**2) * 3600. *24.
+    vor = 0.5*(dudy-dvdx) * 3600. *24.
+    eps_tot = np.sqrt(div**2+shr**2)
+    eps_tot = np.where((aice[1:-1,1:-1]>0) & (aice[1:-1,1:-1]<=1), eps_tot, np.nan)
+    
+    # generate arctic basin mask
+    mask = ((((lon > -120) & (lon < 100)) & (lat >= 80)) |
+            ((lon <= -120) & (lat >= 70)) |
+            ((lon >= 100) & (lat >= 70)))
+    index_x = np.where(np.sum(mask[1:-1,1:-1],axis=0)>0)
+    index_y = np.where(np.sum(mask[1:-1,1:-1],axis=1)>0)
+    
+    # apply mask and shrink arrays
+    eps_tot = np.where(mask[1:-1,1:-1], eps_tot, np.nan)
+    eps_tot = eps_tot[max([0,index_y[0][0]-1]):index_y[0][-1]+2,
+                      max([0,index_x[0][0]-1]):index_x[0][-1]+2]
+    eps_tot[0,:] = np.nan; eps_tot[-1,:] = np.nan
+    eps_tot[:,0] = np.nan; eps_tot[:,-1] = np.nan
+    eps_tot[1,:] = np.nan; eps_tot[-2,:] = np.nan
+    eps_tot[:,1] = np.nan; eps_tot[:,-2] = np.nan
+    aice = aice[1:-1,1:-1]
+    aice = aice[max([0,index_y[0][0]-1]):index_y[0][-1]+2,
+                max([0,index_x[0][0]-1]):index_x[0][-1]+2]
+
+    # calculate spatial scaling correction factor and scale the detection parameters
+    corfac = 12.5e3/np.mean([np.nanmean(dxu),np.nanmean(dyu)])/float(red_fac)
+    max_kernel = max_kernel*(1+corfac)*0.5
+    min_kernel = min_kernel*(1+corfac)*0.5
+    
+    # use_eps: use the deformation rate field for lkf detection
+    # not use_eps: apply logarithm and histogram equalization to the
+    # deformation rate before the lkf detection
+    if use_eps:
+        proc_eps = eps_tot
+    else:
+        proc_eps = np.log(eps_tot)
+    proc_eps[~np.isfinite(proc_eps)] = np.NaN
+    if not use_eps:
+        proc_eps = hist_eq(proc_eps)
+
+    # apply dog filter
+    lkf_detect = DoG_leads(proc_eps,max_kernel,min_kernel)
+
+    if plot:
+        fig = plt.figure(figsize=(10,8))
+        axs = [fig.add_subplot(2, 2, n, projection=cartopy.crs.Orthographic(0, 90)) for n in range(1,5)]
+
+        for ax, data, title, vmax_ in zip(
+            axs[:3], [eps_tot, lkf_detect], ['total deformation', 'difference of gaussian filter (DoG)'], vmax
+        ):
+            ax.pcolormesh(data,vmin=0, vmax=vmax_)
+            ax.set_title(title, fontsize=16)
+
+    # apply threshold, i.e. mark as lkf pixels if value after dog filter > dog_thres
+    lkf_detect = (lkf_detect > dog_thres).astype('float')
+    lkf_detect[~np.isfinite(proc_eps)] = np.NaN
+    lkf_detect = (lkf_detect > 0)
+
+    # calculate average total deformation
+    eps_tot = np.nanmean(np.stack(eps_tot),axis=0)
+
+    # apply morphological thinning
+    if skeleton_kernel==0:
+        lkf_thin =  skimage.morphology.skeletonize(lkf_detect).astype('float')
+    else:
+        lkf_thin = skeleton_along_max(eps_tot,lkf_detect,kernelsize=skeleton_kernel).astype('float')
+        lkf_thin[:2,:] = 0.; lkf_thin[-2:,:] = 0.
+        lkf_thin[:,:2] = 0.; lkf_thin[:,-2:] = 0.
+
+    # this averages the ice concentration over 2 grid cells in every direction
+    # (originally implemented for np.where(aice_mean>aice_thresh, ...)
+    # it is not used because it does not change the result and
+    # slows down the calculation way too much)
+    #aice_mean = aice.copy()
+    #aice_mean[2:-2,2:-2] = [[np.nanmean(aice[i-2:i+3,j-2:j+3])
+    #                        for j in range(2,np.shape(aice)[1]-2)]
+    #                       for i in range(2,np.shape(aice)[0]-2)
+    #                      ]
+
+    lkf_thin = np.where(aice>aice_thresh,lkf_thin,0)    
+
+    if plot:
+        for ax, data, title in zip(
+            axs[2:], [lkf_detect, lkf_thin], ['threshold applied to DoG', 'morphological thinning']
+        ):
+            ax.pcolormesh(data,vmin=0, vmax=1, cmap='Greys')
+            ax.set_title(title, fontsize=16)
+
+        fig.tight_layout()
+        plt.show()
+
+    return lkf_thin
