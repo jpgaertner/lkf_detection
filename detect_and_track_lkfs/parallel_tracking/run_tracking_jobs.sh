f54fa764cfe5066daebac148595ea8b8401e5544
jpgaertner <jph.gaertner@gmail.com>
2024-09-24 12:10:53 +0200

    update sed commands

--- detect_and_track_lkfs/parallel_tracking/run_tracking_jobs.sh
+++ detect_and_track_lkfs/parallel_tracking/run_tracking_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-years=({2013..2020})
-step=20
-res="4km"
+years=(2097)
+step=10
+res="1km"
 
 # needed for selecting the number of tasks and nodes in the job script
 round_up() {
@@ -15,20 +15,20 @@ for year in "${years[@]}"; do
     for (( day = 0; day <= 365; day += $step )); do
     
         # select dataset for the individual job 
-        sed -i "10s/.*/res = '${res}'/" track_lkfs_chunks.py
-        sed -i "11s/.*/year = $year/" track_lkfs_chunks.py
-        sed -i "12s/.*/lower = $day/" track_lkfs_chunks.py
-        sed -i "13s/.*/step = $step/" track_lkfs_chunks.py
+        sed -i "s|\(res = \).*|\1'$res'|" track_lkfs_chunks.py
+        sed -i "s|\(year = \).*|\1$year|" track_lkfs_chunks.py
+        sed -i "s|\(day = \).*|\1$day|" track_lkfs_chunks.py
+        sed -i "s|\(step = \).*|\1$step|" track_lkfs_chunks.py
 
         # save as new file
         cp track_lkfs_chunks.py /work/bk1377/a270230/python_scripts/track_lkfs_chunks_${year}_${day}.py
     done
     
     # adjust job script and submit it
-    sed -i "2s/.*/#SBATCH --job-name=t${year}/" tracking_jobs.sh
-    sed -i "14s/.*/year=${year}/" tracking_jobs.sh
-    sed -i "15s/.*/step=${step}/" tracking_jobs.sh
+    sed -i "s|\(#SBATCH --job-name=t\).*|\1$year|" tracking_jobs.sh
+    sed -i "s|\(year=\).*|\1$year|" tracking_jobs.sh
+    sed -i "s|\(step=\).*|\1$step|" tracking_jobs.sh
     ntasks=$(round_up 365/$step)
-    sed -i "6s/.*/#SBATCH --ntasks=$ntasks/" tracking_jobs.sh
+    sed -i "s|\(#SBATCH --ntasks=\).*|\1$ntasks|" tracking_jobs.sh
     sbatch tracking_jobs.sh
 done
