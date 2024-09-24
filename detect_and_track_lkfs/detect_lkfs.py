91433226d56b487928b1c1dcc6e2cd60e6140e8e
jpgaertner <jph.gaertner@gmail.com>
2024-09-24 11:48:11 +0200

    clean up

--- detect_and_track_lkfs/detect_lkfs.py
+++ detect_and_track_lkfs/detect_lkfs.py
@@ -1,22 +1,25 @@
 import numpy as np
 import sys
-sys.path.insert(1, '/home/a/a270230/LKF/lkf_tools/lkf_tools/')
+sys.path.insert(1, '../../lkf_tools/lkf_tools/')
 from dataset import *
 import warnings
 warnings.filterwarnings('ignore')
 
+
+res = '4km'
+
 path = '/work/bk1377/a270230/'
-path_nc   = path + 'interpolated_fesom_output/1km/'
+path_nc   = path + f'interpolated_fesom_output/{res}/'
 path_lkfs = path + 'lkfs/'
 path_ds   = path + 'datasets/'
 
 year = 2013
 
-lkf_data = process_dataset(netcdf_file = path_nc + f'{year}_1km.nc',
+lkf_data = process_dataset(netcdf_file = path_nc + f'{year}_{res}.nc',
                            output_path = path_lkfs,
                            dog_thres = 0.01,
                            t_red = 1)
 
-lkf_data.detect_lkfs(indexes=np.arange(29,100,1), use_eps=True)
+lkf_data.detect_lkfs(indexes=np.arange(365))
 
-np.save(path_ds + f'ds_{year}_1km', [lkf_data])
+np.save(path_ds + f'ds_{year}_{res}', [lkf_data])
