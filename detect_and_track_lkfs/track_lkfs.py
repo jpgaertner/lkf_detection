ae67f8d4cc00f0d496f04a427e0d380df489ac8f
jpgaertner <jph.gaertner@gmail.com>
2024-09-24 11:49:55 +0200

    track_lkfs.py

--- detect_and_track_lkfs/track_lkfs.py
+++ detect_and_track_lkfs/track_lkfs.py
@@ -1,16 +1,20 @@
 import numpy as np
 import sys
-sys.path.insert(1, '/home/a/a270230/LKF/lkf_tools/lkf_tools/')
+sys.path.insert(1, '../../lkf_tools/lkf_tools/')
 from dataset import *
 import warnings
 warnings.filterwarnings('ignore')
+
+
+res = '4km'
 
 path = '/work/bk1377/a270230/'
 path_ds = path + 'datasets/'
 
 year = 2013
-res = '4km'
 
 lkf_data = np.load(path_ds + f'ds_{year}_{res}.npy', allow_pickle=True)[0]
 
 lkf_data.track_lkfs(indexes=np.arange(365))
+
+np.save(path_ds + f'ds_{year}_{res}', [lkf_data])
