0465a72c85f89c4b33122e2a1e2f1d946f6f93e5
jpgaertner <jph.gaertner@gmail.com>
2024-09-24 12:01:48 +0200

    different config, not important

--- detect_and_track_lkfs/parallel_detection/detection_jobs.sh
+++ detect_and_track_lkfs/parallel_detection/detection_jobs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
-#SBATCH --job-name=d2016
+#SBATCH --job-name=d2020
 #SBATCH --account=bk1377
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=21
 #SBATCH -t 8:00:00
 #SBATCH --output=output.txt
 
 
 export OMP_NUM_THREADS=1
 
-year=2016
-startdays_of_chunks=(235 )
+year=2020
+startdays_of_chunks=(29 87 146 168 180 190 200 210 220 230 240 250 260 270 280 290 300 310 320 330 345 )
 
 for day in "${startdays_of_chunks[@]}"; do
     python3 /work/bk1377/a270230/python_scripts/detect_lkfs_chunks_${year}_${day}.py &
